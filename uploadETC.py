8edf93365059a33124f997370d4989a09c99a5b4
EricLarmanou <118964196+EricLarmanou@users.noreply.github.com>
2023-10-29 13:48:37 +0100

    Update uploadETC.py

--- uploadETC.py
+++ uploadETC.py
@@ -13,68 +13,69 @@ import hashlib
 import requests
 import logging
 
-#-------------------------------------------------------------------------------------------------------------------
-# VARIABLES to specify
+#Settings-----------------------------------------------------------------------------------------------------------
+#station ID
+#ex: stationID = 'FA-Lso'
+stationID = 'FA-Lso'
 
-# Specify your Station ID
-# eg:  station="FA-Lso"
-stationID = "FA-Lso"
+#passoword
+#ex: pwd = 'p4ssw0rd'
+pwd = 'p4ssw0rd'
 
-# Specify your Passoword here
-# eg:   passphrase="p4ssw0rd"
-pwd = "p4ssw0rd"
-
-# specify the mask of the data files
-folders = [r"C:\precip\*.dat'",
-           r"C:\EC\*.zip'",
-           r"C:\met\*.dat'",
-           r"C:\SSN\*.dat'",
-           r"C:\SSS\*.dat'"]
-#-------------------------------------------------------------------------------------------------------------------
+#path mask(s) of the data files
+path_masks = [r'C:\precip\*.dat',
+           r'C:\EC\*.zip',
+           r'C:\met\*.dat',
+           r'C:\SSN\*.dat',
+           r'C:\SSS\*.dat']
+#Initalization------------------------------------------------------------------------------------------------------
 #open a log file
 logging.basicConfig(level=logging.INFO,
                     format='%(asctime)s> %(message)s',
                     handlers=[logging.FileHandler('uploadETC.log', mode='w'), logging.StreamHandler()],
                     force=True)
-#-------------------------------------------------------------------------------------------------------------------
-#loop over all folders
+#Processing---------------------------------------------------------------------------------------------------------
+#loop over path masks
 for path_mask in path_masks:
-    #write current directory on screen
+    #log current path mask
     logging.info(' --- Search for files matching: ' + path_mask + ' ---')
 
-    # Find all files in that folder
+    #list the files matching the path mask
     files = sorted(glob(path_mask))
 
-    #loop over all files
+    #loop over files
     NbOk = 0
     for file in files:
+        #get the current file name
         basename = os.path.basename(file)
-        #write current filename on the screen
+
+        #log the current filename
         logging.info(basename)
 
-        #calculate the checksum of the current file
+        #open the current file and calculate its checksum
         fid = open(file,'rb')
         checksum = hashlib.md5(fid.read()).hexdigest().upper()
         fid.seek(0)
 
-        #write checksum of current file on the screen
+        #log the current file checksum
         logging.info(checksum)
 
-        #generate full url for current file
+        #generate the url to upload the current file
         url = 'https://' + stationID + ':' + pwd + '@data.icos-cp.eu/upload/etc/' + checksum + '/' + basename
 
         #write full url for current file on screen and in log file
         logging.info(url)
 
-        # upload (put) current file to server
+        #upload (put) the current file to the server
         result = requests.put(url, data = fid)
 
         if result.ok:
             NbOk += 1
 
+        #log the result of the request
         logging.info(result.text)
 
-        #close the file
+        #close the current file
         fid.close()
         logging.info('-------------------------')
 
